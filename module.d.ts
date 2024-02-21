c9a659d8ac0a8d31eff6af08f3ac1a23d16ef97b
JacopoPatroclo <jacopo.patroclo.martinelli@gmail.com>
2024-02-21 23:23:23 +0100

    fix: module missing default export

--- module.d.ts
+++ module.d.ts
@@ -1,4 +1,9 @@
 declare module 'fastify-better-flash' {
+  import { FastifyPluginCallback } from 'fastify'
+
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface FlashSessionType {}
+
+  const betterflash: FastifyPluginCallback<() => string>
+  export default betterflash
 }
